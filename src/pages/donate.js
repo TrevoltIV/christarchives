59e0713ba0799c922f7eda330fd8c70047c2d7e1
TrevoltIV <kgk1999@gmail.com>
2023-09-10 03:09:35 -0400

    Donate BTC

--- src/pages/donate.js
+++ src/pages/donate.js
@@ -17,7 +17,17 @@ export default function Donate() {
         <div className={styles.container}>
             <Header />
             <div className={styles.donate}>
-                Donate page - under construction, please come back later!
+                Donate page - under construction.
+
+                <h2>
+                    If you would like to donate via Bitcoin, you can send to the address below. Otherwise, please come back in a few days when I have implemented payments.
+                    All donations go to either advertising or charity, no profits will be made whatsoever. Thanks!
+                </h2>
+                <br />
+                <br />
+                <h3>
+                    &nbsp;BTC ADDRESS: &nbsp;bc1q7gazhsrmxfmyjqj7hdfg6eemf8hy59fw2kzdyl
+                </h3>
             </div>
         </div>
     )
